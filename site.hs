c4a67334fbae9c0646eab34bfcbdeb593f31e52c
Ben Pski <ben@pepski.com>
2019-03-17 20:27:13 -0600

    Add deploy command.

--- site.hs
+++ site.hs
@@ -6,7 +6,11 @@ import           Hakyll
 
 --------------------------------------------------------------------------------
 
+<<<<<<< Updated upstream
 deployConfig = defaultConfiguration {deployCommand =  "git stash && git checkout develop && stack exec website clean && stack exec website build && git fetch --all && git checkout -b master --track origin/master && cp -a _site/. . && git add -A && git commit -m \"Publish.\" && git push origin master:master && git checkout develop && git branch -D master && git stash pop" }
+=======
+deployConfig = defaultConfiguration {deployCommand =  "git stash && git checkout develop && stack exec myblog clean && stack exec myblog build && git fetch --all && git checkout -b master --track origin/master && cp -a _site/. . && git add -A && git commit -m \"Publish.\" && git push origin master:master && git checkout develop && git branch -D master && git stash pop" }
+>>>>>>> Stashed changes
 
 
 main :: IO ()
